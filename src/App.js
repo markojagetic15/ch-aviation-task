7552813e20c3b7a6f6feaee2aeb30cd8e6514090
Marko Jagetić <marko.jagetic@asynclabs.co>
2022-11-10 10:20:21 +0100

    Add main layout component

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import DropdownMenu from "./DropdownMenu";
+import OptionsForm from "./OptionsForm";
+
+const App = () => {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [hasError, serError] = useState(false);
+  const [isDeleted, setDeleted] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (selectedOption === "") {
+      serError(true);
+      return;
+    }
+    serError(false);
+    setOptions((options) => [...options, selectedOption]);
+    e.target.reset();
+  };
+
+  const handleDelete = (index) => {
+    let array = [...options];
+    if(index > -1) {
+      array.splice(index, 1)
+      setOptions(array)
+    }
+    setDeleted(true);
+  }
+
+  return (
+    <div className="App">
+      <div className="main-container">
+        <div>
+          <h3>Choose an option</h3>
+          <DropdownMenu options={options} isDeleted={isDeleted}/>
+        </div>
+        <OptionsForm
+          handleChange={(e) => setSelectedOption(e.target.value)}
+          handleSubmit={handleSubmit}
+          handleDelete={handleDelete}
+          hasError={hasError}
+          options={options}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
